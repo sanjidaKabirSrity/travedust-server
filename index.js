436c0fd987680c582db345d34febb4d8411aa468
sanjidaKabirSrity <sanjida.kabir.srity@gmail.com>
2021-10-29 19:44:24 +0600

    heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 // middleWare 
 app.use(cors());
