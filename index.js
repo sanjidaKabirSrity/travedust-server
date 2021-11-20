e69c9064016de02de11b24e3f189dbe72fced714
sanjidaKabirSrity <sanjida.kabir.srity@gmail.com>
2021-11-20 18:31:03 +0600

    update

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
-// MiddleWare 
+//MiddleWare 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bonkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 async function run() {
     try {
